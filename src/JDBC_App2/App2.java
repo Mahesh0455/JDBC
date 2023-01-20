1a1904965807fdf4567c379c2ea31a86de602d78
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-20 20:22:01 +0530

    App2

--- src/JDBC_App2/App2.java
+++ src/JDBC_App2/App2.java
@@ -1,9 +1,17 @@
 package JDBC_App2;
 
+import java.io.*;
+
 public class App2 {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello Mahesh");
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		FileInputStream fis=new FileInputStream(new File("C:\\Users\\MPawar\\Desktop\\mahesh\\learnings\\java\\git_projects\\JDBC","db.properties"));
+		
+		
+		
+		
 	}
 
 }
